fea40b2d391053261f62cac7a878f9ca5ddd002c
Gxbriel <eglig@bzz.ch>
2022-04-04 16:35:52 +0200

    basics

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const ws = require('ws');
 
 const app = express();
+const wsServer = new ws.Server({ noServer: true });
 const port = 8000;
 
+// <== HTTP ==>
+
 app.get('/', (req, res) => {
-    res.status(200).send('du hund');
+    res.sendFile(__dirname + '/frontend/index.html');
 });
 
-app.listen(port, (e) => {
+// <== WebSocket ==>
+
+wsServer.on('connection', (socket) => {
+    socket.on('message', (message) => {
+        socket.send('ehllo');
+        console.log(message.toString());
+    });
+});
+
+const server = app.listen(port, (e) => {
     if (e) console.log('> [HTTP] Error: ', e);
     console.log('> [HTTP] server listening on http://localhost:' + port);
+});
+
+server.on('upgrade', (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, (socket) => {
+        wsServer.emit('connection', socket, request);
+    });
 });
